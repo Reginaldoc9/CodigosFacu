ee9690a34dd58ef6e1635d28f98eee0a1a78ab26
Reginaldoc9 <32533091+Reginaldoc9@users.noreply.github.com>
2017-12-12 21:16:53 -0200

    Update Automovel.java

--- ArrayLixo1/ArrayLixo/src/arraylixo/Automovel.java
+++ ArrayLixo1/ArrayLixo/src/arraylixo/Automovel.java
@@ -1,23 +1,27 @@
 package arraylixo;
 
 class Automovel {
+    //Atributos da classe
     private String marca;
     private String modelo;
     private double motor;
 
+    //toString: Para retornar os valores dos atributos da classe em String
     @Override
     public String toString() {
         return "Automovel{" + "marca=" + marca + ", modelo=" + modelo + ", motor=" + motor + '}'+"\n";
     }
-    
+   //Construtor da classe:  serve para obrigar o usuario a inserir os valores dos atributos do objeto, o "this." serve para indicar que o construtor faz referência ao atributo do objeto
     public Automovel(String marca, String modelo, double motor){
+        //this serve para transportar o valor inserido
         this.marca = marca;
         this.modelo = modelo;
         this.motor = motor;
         
         
     }
-        
+        //get and set: Eles servem para pegarmos informações  de variáveis da classe que são definidas como 'private',
+    //porém esses método são definidos como 'public'.
 
     public String getMarca() {
         return marca;
